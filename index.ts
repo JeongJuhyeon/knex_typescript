b71d1893193ccdfe0faba695d9bcab022a96f90e
jjh <j.h.jeong@outlook.com>
2021-04-02 15:42:36 +0900

    package.json

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
 import knex from './knex';
-console.log('hello world 2');
 
 async function helloWorld() {
   const a = await knex('Album').select('*');
   console.log(a[0].Title);
-  const a2 = await knex('Album').insert({ Title: '3' });
+  //   const a2 = await knex('Album').insert({ Title: '3' });
   const tracks = await knex('Track').select('*');
 }
 
